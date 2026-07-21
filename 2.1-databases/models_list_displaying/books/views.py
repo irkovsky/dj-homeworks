f9806bf5f291b72a01a1e27528fb2993932e7434
irkovsky <irkovsky@gmail.com>
2026-07-21 16:26:15 +0300

    main draft for books project

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 from .models import Book
 
 
-def books_view(request):
+def books_view(request, pub_date=None):
     template = 'books/books_list.html'
     
-    books = Book.objects.all()
+    if not pub_date:
+        books = Book.objects.all().order_by('pub_date')
+        context = {
+            'books': books
+        }
+        return render(request, template, context)
+    
+    books = Book.objects.all().filter(pub_date=pub_date)
+    # ошибка формата
+    
+    prev_book = Book.objects.all().filter(pub_date__lt=pub_date).order_by('-pub_date').first()
+    prev_date = prev_book.pub_date if prev_book else None
+    
+    next_book = Book.objects.all().filter(pub_date__gt=pub_date).order_by('pub_date').first()
+    next_date = next_book.pub_date if next_book else None 
     
     context = {
-        'books': books
+        'books': books,
+        'prev_date': prev_date,
+        'next_date': next_date
     }
+
     return render(request, template, context)
